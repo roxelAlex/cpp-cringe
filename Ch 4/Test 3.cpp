ba28114570542b02ebacd26cb8965a579906fe1f
roxelAlex <roxelalex@yandex.ru>
2023-09-02 12:50:35 +0500

    Решить третий тест главы 4

---
+++ Ch 4/Test 3.cpp
@@ -0,0 +1,19 @@
+// Напишите перечисление со следующими перечислителями: 
+// ogre, goblin, skeleton, orc и troll.
+
+enum Monster
+{
+    MONSTER_OGRE,
+    MONSTER_GOBLIN,
+    MONSTER_SKELETON,
+    MONSTER_ORC,
+    MONSTER_TROLL,
+};
+
+// Объявите переменную перечисляемого типа,
+// который вы определили в задании №1, и присвойте ей значение ogre.
+
+int main()
+{
+    Monster monster = MONSTER_OGRE;
+}
