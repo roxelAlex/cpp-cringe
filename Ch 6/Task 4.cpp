c590c69e1666b399e102064441dd6c56e22393d4
roxelAlex <roxelalex@yandex.ru>
2023-10-12 10:30:46 +0500

    Решить задание 4 главы 6

---
+++ Ch 6/Task 4.cpp
@@ -0,0 +1,25 @@
+/*
+Напишите функцию для вывода строки C-style символ за символом.
+Используйте указатель для перехода и вывода каждого символа поочерёдно.
+Остановите вывод при столкновении с нуль-терминатором.
+В функции main() протестируйте строку Hello, world!.
+*/
+
+#include <iostream>
+
+void outputCStyle(char *array)
+{
+    while (*array != 0)
+    {
+        std::cout << *array << "\n";
+        array++;
+    }
+}
+
+int main()
+{
+    char string[] = "Hello, world";
+
+    outputCStyle(string);
+    return 0;
+}
